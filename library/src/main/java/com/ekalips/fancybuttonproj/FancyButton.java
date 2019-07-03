9b37d61a5e51cf3d5f07a3ad81ec1a57bc912ee5
MuhammadNashat <muhammadnashat22@gmail.com>
2019-07-03 15:54:00 +0200

    1 - Adding BorderRadius and BorderRadius with Stroke to the Button by Adding a Background Drawable for it
    (You shouldn't add Background Drawable if it doesn't have the BorderRadius
    and if you gonna add a solid color to it ... make the same color as fillColor attribute
    and if you gonna add Stroke to a BorderRadius don't add it to the Button Stroke Attribute)
    
    2 -  Adding TextAppearance , TextFont
    put the font in Assets Folder
    then call its Path
    so if u gonna put it in a folder called Fonts inside Assets Folder
    call it by(Fonts/font_name.ttf)

--- library/src/main/java/com/ekalips/fancybuttonproj/FancyButton.java
+++ library/src/main/java/com/ekalips/fancybuttonproj/FancyButton.java
@@ -5,10 +5,12 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
@@ -21,6 +23,9 @@ import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 import me.zhanghai.android.materialprogressbar.IndeterminateCircularProgressDrawable;
 import me.zhanghai.android.materialprogressbar.MaterialProgressBar;
 
@@ -30,11 +35,11 @@ import me.zhanghai.android.materialprogressbar.MaterialProgressBar;
 public class FancyButton extends FrameLayout {
 
     interface AnimationEndListener {
+
         void animationEnded();
     }
 
     private static final String TAG = FancyButton.class.getSimpleName();
-
 
     public FancyButton(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -103,18 +108,39 @@ public class FancyButton extends FrameLayout {
         int textColor = ta.getColor(R.styleable.FancyButton_f_textColor, Color.BLACK);
         int progressColor = ta.getColor(R.styleable.FancyButton_f_progressColor, Color.BLACK);
         int strokeWidth = ta.getInt(R.styleable.FancyButton_f_strokeWidth, -1);
-        if (strokeWidth == -1)
-            strokeWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, context.getResources().getDisplayMetrics());
+
+        //Adding new Attribute Background for BorderRadius
+        /*
+         * If the Background is not Null ... It must have BorderRadius and Color
+         * If it doesnt Have a BorderRadius then no need to add it
+         * if u gonna add a stroke in the background .. then u dont need to add it with_f_strokeWidth or f_strokeCollor
+         * */
+
+        int background = ta.getResourceId(R.styleable.FancyButton_f_background, -1);
+        //TextAppearance
+        int textAppearance = ta.getResourceId(R.styleable.FancyButton_f_textAppearance, -1);
+
+        //Textfont from Assets
+        String textFont = ta.getString(R.styleable.FancyButton_f_textFont);
+
+        if (strokeWidth == -1) {
+            strokeWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4,
+                context.getResources().getDisplayMetrics());
+        }
 
         boolean capsText = ta.getBoolean(R.styleable.FancyButton_f_capsText, true);
         float textSize = ta.getDimensionPixelSize(R.styleable.FancyButton_f_textSize, -1);
-        if (textSize == -1)
-            textSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, context.getResources().getDisplayMetrics());
-        if (capsText && text != null)
+        if (textSize == -1) {
+            textSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16,
+                context.getResources().getDisplayMetrics());
+        }
+        if (capsText && text != null) {
             text = text.toUpperCase();
+        }
         String temp = ta.getString(R.styleable.FancyButton_f_btnStyle);
-        if (temp != null)
+        if (temp != null) {
             style = Integer.parseInt(temp);
+        }
         hideAfterCollapse = ta.getBoolean(R.styleable.FancyButton_f_hideFillAfterCollapse, true);
         ta.recycle();
 
@@ -122,20 +148,60 @@ public class FancyButton extends FrameLayout {
         view.setClickable(false);
         view.setFocusable(false);
         view.setTextColor(textColor);
+
         view.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         view.setLayoutParams(params);
         view.setText(text);
+        //Checking if background is Null  or not
+        if (background != -1) {
+            view.setBackground(getResources().getDrawable(background));
+        }
+
+        //Checking if textFont is Null
+        if (textFont != null) {
+            AssetManager manager = getResources().getAssets();
+            InputStream inputStream = null;
+            try {
+                inputStream = manager.open(textFont);
+                Typeface face = Typeface.createFromAsset(context.getAssets(),
+                    textFont);
+                view.setTypeface(face);
+            } catch (IOException ex) {
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+
+        //Checking if textAppearance is not null
+        if (textAppearance != -1)
+            try {
+                view.setTextAppearance(context,textAppearance);
+            } catch (Exception e)
+            {
+                Log.e("Error",e.getMessage());
+            }
+
         this.addView(view);
 
         bar = new MaterialProgressBar(context);
-        FrameLayout.LayoutParams barParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        FrameLayout.LayoutParams barParams = new FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         barParams.gravity = Gravity.CENTER;
         bar.setLayoutParams(barParams);
         bar.setIndeterminate(true);
 
         // fixes pre-Lollipop progressBar indeterminateDrawable tinting
-        IndeterminateCircularProgressDrawable drawable = new IndeterminateCircularProgressDrawable(context);
+        IndeterminateCircularProgressDrawable drawable = new IndeterminateCircularProgressDrawable(
+            context);
         drawable.setTint(progressColor);
         bar.setIndeterminateDrawable(drawable);
 
@@ -164,13 +230,21 @@ public class FancyButton extends FrameLayout {
         super.onDraw(canvas);
 
         if (state == State.normal) {
-            if (style == 0)
-                canvas.drawPath(Utils.composeRoundedRectPath(initPadd, initPadd, canvas.getWidth() - initPadd, canvas.getHeight() - initPadd, initPadd), strokePaint);
-            else if (style == 1)
-                canvas.drawPath(Utils.composeRoundedRectPath(initPadd, initPadd, canvas.getWidth() - initPadd, canvas.getHeight() - initPadd, initPadd), fillPaint);
-            else if (style == 2) {
-                canvas.drawPath(Utils.composeRoundedRectPath(initPadd, initPadd, canvas.getWidth() - initPadd, canvas.getHeight() - initPadd, initPadd), fillPaint);
-                canvas.drawPath(Utils.composeRoundedRectPath(initPadd, initPadd, canvas.getWidth() - initPadd, canvas.getHeight() - initPadd, initPadd), strokePaint);
+            if (style == 0) {
+                canvas.drawPath(Utils
+                    .composeRoundedRectPath(initPadd, initPadd, canvas.getWidth() - initPadd,
+                        canvas.getHeight() - initPadd, initPadd), strokePaint);
+            } else if (style == 1) {
+                canvas.drawPath(Utils
+                    .composeRoundedRectPath(initPadd, initPadd, canvas.getWidth() - initPadd,
+                        canvas.getHeight() - initPadd, initPadd), fillPaint);
+            } else if (style == 2) {
+                canvas.drawPath(Utils
+                    .composeRoundedRectPath(initPadd, initPadd, canvas.getWidth() - initPadd,
+                        canvas.getHeight() - initPadd, initPadd), fillPaint);
+                canvas.drawPath(Utils
+                    .composeRoundedRectPath(initPadd, initPadd, canvas.getWidth() - initPadd,
+                        canvas.getHeight() - initPadd, initPadd), strokePaint);
             }
             destLeft = (this.getRight() - this.getLeft()) / 2 - trueSixtee;
             destRight = (this.getRight() - this.getLeft()) / 2 + trueSixtee;
@@ -183,8 +257,10 @@ public class FancyButton extends FrameLayout {
             animator.start();
         } else if (state == State.shrieked) {
             if (!hideAfterCollapse) {
-                if (style == 1 || style == 2)
-                    canvas.drawCircle(destRight - ((destRight - destLeft) / 2), destBot - ((destBot - destTop) / 2), circleR / 2, fillPaint);
+                if (style == 1 || style == 2) {
+                    canvas.drawCircle(destRight - ((destRight - destLeft) / 2),
+                        destBot - ((destBot - destTop) / 2), circleR / 2, fillPaint);
+                }
 
             }
             ObjectAnimator animator = ObjectAnimator.ofFloat(bar, "alpha", 0, 1);
@@ -192,26 +268,65 @@ public class FancyButton extends FrameLayout {
             animator.start();
             bar.setVisibility(VISIBLE);
         } else {
-            if (state == State.shrink)
-                if (style == 0)
-                    canvas.drawPath(Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH), (float) (canvas.getWidth() - nowPadW), (float) (canvas.getHeight() - nowPadH), (float) nowRad), strokePaint);
-                else if (style == 1)
-                    canvas.drawPath(Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH), (float) (canvas.getWidth() - nowPadW), (float) (canvas.getHeight() - nowPadH), (float) nowRad), fillPaint);
-                else if (style == 2) {
-                    canvas.drawPath(Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH), (float) (canvas.getWidth() - nowPadW), (float) (canvas.getHeight() - nowPadH), (float) nowRad), fillPaint);
-                    canvas.drawPath(Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH), (float) (canvas.getWidth() - nowPadW), (float) (canvas.getHeight() - nowPadH), (float) nowRad), strokePaint);
+            if (state == State.shrink) {
+                if (style == 0) {
+                    canvas.drawPath(
+                        Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH),
+                            (float) (canvas.getWidth() - nowPadW),
+                            (float) (canvas.getHeight() - nowPadH),
+                            (float) nowRad), strokePaint);
+                } else if (style == 1) {
+                    canvas
+                        .drawPath(
+                            Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH),
+                                (float) (canvas.getWidth() - nowPadW),
+                                (float) (canvas.getHeight() - nowPadH),
+                                (float) nowRad), fillPaint);
+                } else if (style == 2) {
+                    canvas
+                        .drawPath(
+                            Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH),
+                                (float) (canvas.getWidth() - nowPadW),
+                                (float) (canvas.getHeight() - nowPadH),
+                                (float) nowRad), fillPaint);
+                    canvas
+                        .drawPath(
+                            Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH),
+                                (float) (canvas.getWidth() - nowPadW),
+                                (float) (canvas.getHeight() - nowPadH),
+                                (float) nowRad), strokePaint);
                 }
+            }
             if (state == State.back) {
-                if (style == 0)
-                    canvas.drawPath(Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH), (float) (canvas.getWidth() - nowPadW), (float) (canvas.getHeight() - nowPadH), (float) nowRad), strokePaint);
-                else if (style == 1)
-                    canvas.drawPath(Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH), (float) (canvas.getWidth() - nowPadW), (float) (canvas.getHeight() - nowPadH), (float) nowRad), fillPaint);
-                else if (style == 2) {
-                    canvas.drawPath(Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH), (float) (canvas.getWidth() - nowPadW), (float) (canvas.getHeight() - nowPadH), (float) nowRad), fillPaint);
-                    canvas.drawPath(Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH), (float) (canvas.getWidth() - nowPadW), (float) (canvas.getHeight() - nowPadH), (float) nowRad), strokePaint);
+                if (style == 0) {
+                    canvas.drawPath(
+                        Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH),
+                            (float) (canvas.getWidth() - nowPadW), (float) (canvas.getHeight() - nowPadH),
+                            (float) nowRad), strokePaint);
+                } else if (style == 1) {
+                    canvas
+                        .drawPath(
+                            Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH),
+                                (float) (canvas.getWidth() - nowPadW),
+                                (float) (canvas.getHeight() - nowPadH),
+                                (float) nowRad), fillPaint);
+                } else if (style == 2) {
+                    canvas
+                        .drawPath(
+                            Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH),
+                                (float) (canvas.getWidth() - nowPadW),
+                                (float) (canvas.getHeight() - nowPadH),
+                                (float) nowRad), fillPaint);
+                    canvas
+                        .drawPath(
+                            Utils.composeRoundedRectPath((float) (0 + nowPadW), (float) (0 + nowPadH),
+                                (float) (canvas.getWidth() - nowPadW),
+                                (float) (canvas.getHeight() - nowPadH),
+                                (float) nowRad), strokePaint);
                 }
-                if (bar.getVisibility() != INVISIBLE)
+                if (bar.getVisibility() != INVISIBLE) {
                     bar.setVisibility(INVISIBLE);
+                }
             }
         }
     }
@@ -228,8 +343,9 @@ public class FancyButton extends FrameLayout {
 
     public void expand() {
 //        if (state == State.shrieked){
-        if (state == State.normal)
+        if (state == State.normal) {
             return;
+        }
         if (animator != null) {
             animator.removeAllUpdateListeners();
             animator.cancel();
@@ -251,10 +367,12 @@ public class FancyButton extends FrameLayout {
                 nowPadW -= addWVal;
                 nowRad -= addRad;
                 if (hideAfterCollapse) {
-                    if (strokePaint.getColor() != Color.TRANSPARENT)
+                    if (strokePaint.getColor() != Color.TRANSPARENT) {
                         strokePaint.setAlpha((Integer) valueAnimator.getAnimatedValue());
-                    if (fillPaint.getColor() != Color.TRANSPARENT)
+                    }
+                    if (fillPaint.getColor() != Color.TRANSPARENT) {
                         fillPaint.setAlpha((Integer) valueAnimator.getAnimatedValue());
+                    }
                 }
                 view.setAlpha((int) valueAnimator.getAnimatedValue() / 255f);
             }
@@ -264,8 +382,9 @@ public class FancyButton extends FrameLayout {
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
                 state = State.normal;
-                if (listener != null)
+                if (listener != null) {
                     listener.animationEnded();
+                }
                 isExpanded = true;
             }
         });
@@ -275,8 +394,9 @@ public class FancyButton extends FrameLayout {
 
     public void collapse() {
 //        if (state == State.normal){
-        if (state == State.shrieked)
+        if (state == State.shrieked) {
             return;
+        }
         if (animator != null) {
             animator.removeAllUpdateListeners();
             animator.cancel();
@@ -302,10 +422,12 @@ public class FancyButton extends FrameLayout {
                 nowPadW += addWVal;
                 nowRad += addRad;
                 if (hideAfterCollapse) {
-                    if (strokePaint.getColor() != Color.TRANSPARENT)
+                    if (strokePaint.getColor() != Color.TRANSPARENT) {
                         strokePaint.setAlpha((Integer) valueAnimator.getAnimatedValue());
-                    if (fillPaint.getColor() != Color.TRANSPARENT)
+                    }
+                    if (fillPaint.getColor() != Color.TRANSPARENT) {
                         fillPaint.setAlpha((Integer) valueAnimator.getAnimatedValue());
+                    }
                 }
                 view.setAlpha((int) valueAnimator.getAnimatedValue() / 255f);
             }
@@ -315,8 +437,9 @@ public class FancyButton extends FrameLayout {
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
                 state = State.shrieked;
-                if (listener != null)
+                if (listener != null) {
                     listener.animationEnded();
+                }
             }
         });
         animator.start();
